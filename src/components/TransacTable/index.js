9ccb0a6b66a6c4287bb5933d5b6073b52d7643b2
Ajinkya <patilajink2005@gmail.com>
2025-07-28 16:38:54 +0530

    import csv

--- src/components/TransacTable/index.js
+++ src/components/TransacTable/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import "./style.css";
 import { Select, Table } from 'antd';
+import { FaSearch } from "react-icons/fa";
 
 function TransactionTable({transaction}) {
   const { Option } = Select;
@@ -35,29 +36,36 @@ function TransactionTable({transaction}) {
   },
 ];
 
-  let filterTransaction = transaction.filter((item)=>item.name.toLowerCase().includes(search.toLowerCase()) 
+  let filterTransaction = transaction.filter((item)=>item.tag.toLowerCase().includes(search.toLowerCase()) 
                           && item.type.includes(typeFilter));
 
   return (
           <>
-    <div className="filter-bar">
-      <input
-        value={search}
-        onChange={(e) => setSearch(e.target.value)}
-        placeholder="search by name"
-      />
-      <Select
-        className="select-input"
-        onChange={(value) => setTypeFilter(value)}
-        value={typeFilter}
-        placeholder="Filter"
-        allowClear
-        style={{ minWidth: 120 }}
-      >
-        <Option value="">All</Option>
-        <Option value="income">Income</Option>
-        <Option value="expense">Expense</Option>
-      </Select>
+    <div className="filter-bar-outer">
+      <div className="filter-bar-center">
+        <div className="search-input-wrapper">
+          <FaSearch className="search-icon" />
+          <input
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="search by Tag"
+            className="search-input"
+          />
+        </div>
+        <Select
+          className="select-input"
+          onChange={(value) => setTypeFilter(value)}
+          value={typeFilter}
+          placeholder="Filter"
+          allowClear
+          style={{ minWidth: 120 }}
+        >
+          <Option value="">All</Option>
+          <Option value="income">Income</Option>
+          <Option value="expense">Expense</Option>
+        </Select>
+      </div>
+      <button className="import-csv-btn">Import CSV</button>
     </div>
     <Table className="tt" dataSource={filterTransaction} columns={columns} />
   </>
